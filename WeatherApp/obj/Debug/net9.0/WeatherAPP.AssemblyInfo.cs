1ffc38f9c8c03cfb4ae5deda303e1301311c1fc4
Kosinachi <kosisochukwugodwinugochukwu@gmail.com>
2025-09-13 18:29:59 +0100

    Fix Dockerfile case mismatch and clean build artifacts

--- WeatherApp/obj/Debug/net9.0/WeatherAPP.AssemblyInfo.cs
+++ WeatherApp/obj/Debug/net9.0/WeatherAPP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherAPP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+025b20910c502ea7d53b0f9464ab3eb8cd94cb4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherAPP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherAPP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
